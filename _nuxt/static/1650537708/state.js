6fef724e0d0f7cb8b6b8ccefb2ab949e08c2b0bc
pranav34 <pranav@indigitalit.com>
2022-04-21 16:11:50 +0530

    Updates

---
+++ _nuxt/static/1650537708/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpgit363\u002Fnuxt\u002F_nuxt\u002Fstatic\u002F1650537708",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fpgit363\u002Fnuxt\u002F",assetsPath:"\u002Fpgit363\u002Fnuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
